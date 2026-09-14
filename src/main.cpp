be9a3edba7f83840a03c7db8b173cadc21e17cf0
williambcosta <willianbcosta@gmail.com>
2026-09-13 21:44:22 -0300

    Refatora classe SensorPH para melhorar a calibração e leitura de pH, ajustando construtores e métodos de cálculo.

--- src/main.cpp
+++ src/main.cpp
@@ -6,60 +6,61 @@
 #include "SensorTurbidez.h"
 
 /* ----- DEFINICÕES ----- */
-/*
-// Sensores de temperatura
-#define PIN_TEMP_AGUA_BRUTA 1  // Pino do sensor de temperatura da água bruta
-#define PIN_TEMP_TRAT 4        // Pino do sensor de temperatura da água tratada
-#define PIN_TEMP_FINAL 7       // Pino do sensor de temperatura da água final
 
-// Sensores de turbidez
-#define PIN_TBDZ_AGUA_BRUTA 35  // Pino do sensor de turbidez da água bruta
-#define PIN_TBDZ_TRAT 6         // Pino do sensor de turbidez da água tratada
-#define PIN_TBDZ_FINAL 9        // Pino do sensor de turbidez da água final
+// // Sensores de temperatura
+// #define PIN_TEMP_AGUA_BRUTA 34   // Pino do sensor de temperatura da água bruta
+// #define PIN_TEMP_TRAT 35        // Pino do sensor de temperatura da água tratada
+// #define PIN_TEMP_FINAL 36       // Pino do sensor de temperatura da água final
 
-// Sensores de pH
-#define PIN_PH_AGUA_BRUTA 2  // Pino do sensor de pH da água bruta
-#define PIN_PH_TRAT 5        // Pino do sensor de pH da água tratada
-#define PIN_PH_FINAL 8       // Pino do sensor de pH da água final
+// // Sensores de turbidez
+// #define PIN_TBDZ_AGUA_BRUTA 13   // Pino do sensor de turbidez da água bruta
+// #define PIN_TBDZ_TRAT          // Pino do sensor de turbidez da água tratada
+// #define PIN_TBDZ_FINAL 14        // Pino do sensor de turbidez da água final
 
-// Sensores de nível
-#define PIN_SNA_AGUA_BRUTA 13  // Pino do sensor de nível alto da água bruta
-#define PIN_SNB_AGUA_BRUTA 14  // Pino do sensor de nível baixo da água bruta
-#define PIN_SNA_TRAT 22        // Pino do sensor de nível alto da água tratada
-#define PIN_SNB_TRAT 23        // Pino do sensor de nível baixo da água tratada
-#define PIN_SNA_FINAL 24       // Pino do sensor de nível alto da água final
-#define PIN_SNB_FINAL 25       // Pino do sensor de nível baixo da água final
-#define PIN_SNA_EFLU 26        // Pino do sensor de nível alto do efluente
-#define PIN_SNB_EFLU 27        // Pino do sensor de nível baixo do efluente
+// // Sensores de pH
+// #define PIN_PH_AGUA_BRUTA     // Pino do sensor de pH da água bruta
+// #define PIN_PH_TRAT 27        // Pino do sensor de pH da água tratada
+// #define PIN_PH_FINAL 26       // Pino do sensor de pH da água final
 
-// Bombas de transferência
-#define PIN_BOMBA_PT 10  // Pino da bomba de transferência para tratamento
-#define PIN_BOMBA_PF 11  // Pino da bomba de transferência  para final
-#define PIN_BOMBA_PE 12  // Pino da bomba de transferência para efluente
-#define PIN_BOMBA_RE 13  // Pino da bomba de transferência para retorno
+// // Sensores de nível
+// #define PIN_SNA_AGUA_BRUTA 16  // Pino do sensor de nível alto tanque água bruta
+// #define PIN_SNB_AGUA_BRUTA 17  // Pino do sensor de nível baixo tanque água bruta
+// #define PIN_SNA_ATIV 18        // Pino do sensor de nível alto tanque ativos
+// #define PIN_SNB_ATIV 19        // Pino do sensor de nível baixo tanque ativos
+// #define PIN_SNA_FINAL 21       // Pino do sensor de nível alto tanque final
+// #define PIN_SNB_FINAL 22       // Pino do sensor de nível baixo tanque final
+// #define PIN_SNA_EFLU  23       // Pino do sensor de nível alto tanque efluente
+// #define PIN_SNB_EFLU  25       // Pino do sensor de nível baixo tanque efluente
 
-// Solenoides
-#define PIN_SOLENOIDE 14  // Pino do solenoide de controle de fluxo
+// // Bombas de transferência
+// #define PIN_BOMBA_PM1 26  // Pino da bomba de transferência para tanque de ativos
+// #define PIN_BOMBA_PM2 27  // Pino da bomba de transferência para final/efluentes
+// #define PIN_BOMBA_PM3 32  // Pino da bomba de retorno para tanque de armazenamento
 
-// Misturadores
-#define PIN_MISTURADOR_TRAT 15   // Pino do misturador da água tratada
-#define PIN_MISTURADOR_FINAL 16  // Pino do misturador da água final
+// // Solenoides
+// #define PIN_SOL_EFLU 34   // Pino da solenoide de controle de fluxo para o tanque de efluentes
+// #define PIN_SOL_FINAL 4  // Pino da solenoide de controle de fluxo para o tanque final
 
-// Dosadores
-#define PIN_DOSADOR_CLORETO 17      // Pino do dosador de cloreto
-#define PIN_DOSADOR_CARBONATO 18    // Pino do dosador de carbonato
-#define PIN_DOSADOR_HIPOCLORITO 19  // Pino do dosador de hipoclorito
+// // Misturadores
+// #define PIN_RM1_ATIV    // Pino do misturador do tanque de ativos
+// #define PIN_RM2_FINAL   // Pino do misturador do tanque final
 
-// Lâmpadas
-#define PIN_LAMPADA_UV 28  // Pino da lâmpada UV
-*/
+// // Dosadores
+// // #define PIN_DOSADOR_CLORETO       // Pino do dosador de cloreto
+// // #define PIN_DOSADOR_CARBONATO     // Pino do dosador de carbonato
+// // #define PIN_DOSADOR_HIPOCLORITO   // Pino do dosador de hipoclorito
+
+// // Lâmpadas
+// #define PIN_LAMPADA_UV   // Pino da lâmpada UV
+
 // Instancias dos sensores de temperatura
 // SensorTemperatura tempArmazenamento = SensorTemperatura(PIN_TEMP_AGUA_BRUTA); // Sensor de temperatura da água bruta
 // SensorTemperatura tempTrat = SensorTemperatura(PIN_TEMP_TRAT);                // Sensor de temperatura da água em tratamento
 // SensorTemperatura tempFinal = SensorTemperatura(PIN_TEMP_FINAL);              // Sensor de temperatura da água final
 
 // SensorTurbidez ntu = SensorTurbidez(PIN_TBDZ_AGUA_BRUTA, 500, 2.0f, 0.0f);
-SensorPH ph = SensorPH(35, 3.5f, 2.5f);
+
+SensorPH ph = SensorPH(35);  // Sensor de pH da água bruta
 
 /* ----- Configuração inicial ----- */
 void setup() {
@@ -136,6 +137,8 @@ void loop() {
   // Se Armazenamento Nível alto || Efluentes Nível baixo
 
   // ntu.getTurbidez();
-  // Serial.println(analogRead(35)/4095.0f);
-  // delay(500);
+
+  Serial.print("Ph: ");
+  Serial.println(ph.getPH());
+  delay(500);
 }
